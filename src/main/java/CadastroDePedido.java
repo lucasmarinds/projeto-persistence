331073cd03a3c2abacab42602d000e475c0cd9c9
Lucas Marin <lucasmartinsmarin24@gmail.com>
2021-11-17 01:58:20 -0300

    Adição de Itens Pedidos para teste se no relatorio de venda vai trazer em ordem por quantidade vendida

--- src/main/java/CadastroDePedido.java
+++ src/main/java/CadastroDePedido.java
@@ -13,14 +13,19 @@ import java.util.List;
 public class CadastroDePedido {
 
     public static void main(String[] args) {
+
         EntityManager entityManager = JPAUtils.createEntityManager();
         cadastrarProdutos();
         ProdutoDAO produtoDAO = new ProdutoDAO(entityManager);
         Produto produto = produtoDAO.produtoPorNome("HEURI POTTI");
+        Produto produtoDois = produtoDAO.produtoPorNome("Playstation 5");
+        Produto produtoTres = produtoDAO.produtoPorNome("Xbox SS");
 
         Cliente cliente = new Cliente("Lucas","37832870818");
         Pedido pedido = new Pedido(cliente);
-        pedido.adicionarItem(new ItemPedido(3,produto,pedido));
+        pedido.adicionarItem(new ItemPedido(250,produto,pedido));
+        pedido.adicionarItem(new ItemPedido(278,produtoDois,pedido));
+        pedido.adicionarItem(new ItemPedido(321,produtoTres,pedido));
         PedidoDAO pedidoDAO = new PedidoDAO(entityManager);
         ClienteDAO clienteDAO = new ClienteDAO(entityManager);
 
@@ -28,6 +33,7 @@ public class CadastroDePedido {
         clienteDAO.cadastrar(cliente);
         pedidoDAO.cadastrar(pedido);
         entityManager.getTransaction().commit();
+
         BigDecimal valorTotal = pedidoDAO.valorTotalVendido();
         System.out.println("Valor total vendido: "+valorTotal);
 
@@ -43,9 +49,10 @@ public class CadastroDePedido {
     public static void cadastrarProdutos(){
         Categoria celulares = new Categoria("CELULAR");
         Categoria livraria = new Categoria("LIVRO");
-        Produto celular = new Produto("IPHONE XR PRO MAX 28","Celular Bao",new BigDecimal("15500.00"), ModeloCelular.IPONE,celulares);
-        Produto celularDois = new Produto("MOTOROLA G00","Celular BAD",new BigDecimal("1.00"), ModeloCelular.MOTOROLA,celulares);
-        Produto celularTres = new Produto("XIAOMI PITHON","Celular PITUP",new BigDecimal("1222.00"), ModeloCelular.XIAOMI,celulares);
+        Categoria eletronico = new Categoria("ELETRONICOS");
+        Produto xbox = new Produto("Xbox SS","Celular BAO",new BigDecimal("2000.00"), null,eletronico);
+        Produto playstation = new Produto("Playstation 5","Celular BAD",new BigDecimal("4100.00"), null,eletronico);
+        Produto celular = new Produto("IPHONE 11 PRO","Celular TOPPER",new BigDecimal("4500.00"), ModeloCelular.IPONE,celulares);
         Produto livro = new Produto("HEURI POTTI","COMO SER PEPERONI",new BigDecimal("8000.00"), null,livraria);
         EntityManager entityManager = JPAUtils.createEntityManager();
         CategoriaDAO categoriaDAO = new CategoriaDAO(entityManager);
@@ -54,9 +61,10 @@ public class CadastroDePedido {
         entityManager.getTransaction().begin();
         entityManager.persist(celulares);
         entityManager.persist(livraria);
+        entityManager.persist(eletronico);
         entityManager.persist(celular);
-        entityManager.persist(celularDois);
-        entityManager.persist(celularTres);
+        entityManager.persist(playstation);
+        entityManager.persist(xbox);
         entityManager.persist(livro);
         entityManager.getTransaction().commit();
 
