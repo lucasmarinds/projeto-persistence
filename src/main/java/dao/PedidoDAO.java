4e1a9e02fd75cb258b2b068484c4b8de27ea11e4
Lucas Marin <lucasmartinsmarin24@gmail.com>
2021-11-29 11:52:22 -0300

    Adição de comentario sobre a lista de Array de objetos

--- src/main/java/dao/PedidoDAO.java
+++ src/main/java/dao/PedidoDAO.java
@@ -23,6 +23,14 @@ public class PedidoDAO {
         return this.entityManager.createQuery(jpql, BigDecimal.class).getSingleResult();
     }
 
+    /**
+     * É necessário utilizar de um Lista de um Array de Objeto porque como vão ser varios dados de tipos diferentes
+     * pegos nos bancos como quantidade que é integer o nome que é string e a data que é um LocalDate devemos utilizar
+     * da classe mais abstrata possivel que é a Object que todas herdam, e um array porque se formos pensar nesse
+     * relatorio são 3 colunas, Nome/Quantidade/DataUltimaVenda -> logo para cada registro, cada coluna das informações
+     * será uma posição do array cada registro tera 3 valores dentro do array.
+     * @return retorna uma lista de array de object em que registro vai ser um array de object e cada valor do array é uma coluna.
+     */
     public List<Object[]> relatorioDeVenda(){
         String jpql = "SELECT produto.nomeProduto, " +
                 "SUM(item.quantidade), " +
