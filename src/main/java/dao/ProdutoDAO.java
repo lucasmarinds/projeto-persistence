0a1067421eb98f5452f64c7d5a68792e7459dcdc
Lucas Marin <lucasmartinsmarin24@gmail.com>
2022-01-10 15:41:25 -0300

    Utilização de named query, que é uma maneira de deixar uma função de query junto com a entidade.

--- src/main/java/dao/ProdutoDAO.java
+++ src/main/java/dao/ProdutoDAO.java
@@ -8,27 +8,26 @@ import java.util.List;
 public class ProdutoDAO {
     private EntityManager entityManager;
 
-    public ProdutoDAO(EntityManager entityManager){
+    public ProdutoDAO(EntityManager entityManager) {
         this.entityManager = entityManager;
     }
 
-    public void cadastrar(Produto produto){
+    public void cadastrar(Produto produto) {
         this.entityManager.persist(produto);
     }
 
-    public List todosProdutos(){
+    public List todosProdutos() {
         String sql = "SELECT po FROM Produto po";
         return this.entityManager.createQuery(sql).getResultList();
     }
 
-    public Produto produtoPorNome(String nome){
+    public Produto produtoPorNome(String nome) {
         String sql = "SELECT po FROM Produto po WHERE po.nomeProduto = ?1";
         return (Produto) this.entityManager.createQuery(sql).setParameter(1, nome).getSingleResult();
     }
 
-    public Produto produtoPorCategoria(String categoria){
-        String sql = "SELECT po FROM Produto po where po.categoria.nome = :categoria";
-        return (Produto) this.entityManager.createQuery(sql).setParameter("categoria", categoria).getSingleResult();
+    public Produto produtoPorCategoria(String categoria) {
+        return this.entityManager.createNamedQuery("Produto.produtoPorCategoria", Produto.class).setParameter("categoria", categoria).getSingleResult();
     }
 
     public List precosDosProdutos() {
--- src/main/java/modelo/Produto.java
+++ src/main/java/modelo/Produto.java
@@ -7,6 +7,7 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
+@NamedQuery(name = "Produto.produtoPorCategoria", query = "SELECT po FROM Produto po where po.categoria.nome = :categoria")
 public class Produto {
 
     @Id
