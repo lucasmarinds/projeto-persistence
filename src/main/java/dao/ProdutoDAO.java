abc67d3a9f0b53f10f28373c4bd6375faae4c745
lcsmarin <lucasmartinsmarin24@gmail.com>
2022-01-17 14:32:10 -0300

    teste

--- src/main/java/dao/ProdutoDAO.java
+++ src/main/java/dao/ProdutoDAO.java
@@ -3,6 +3,9 @@ package dao;
 import modelo.Produto;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class ProdutoDAO {
@@ -34,5 +37,24 @@ public class ProdutoDAO {
         String sql = "SELECT po.preco FROM Produto po";
         return this.entityManager
                 .createQuery(sql).getResultList();
+    }
+
+    // Colocamos o "1=1" na frente do where só para aceitar que o where tenha uma condição na frente.
+    public List<Produto> buscarPorParametros(String nome, BigDecimal preco){
+        String jpql = "SELECT p FROM Produto p WHERE 1=1";
+        if(nome != null && !nome.isEmpty()){
+            jpql += "AND p.nome = :nome";
+        }
+        if(preco != null){
+            jpql += "AND p.preco = :preco";
+        }
+        TypedQuery<Produto> query = entityManager.createQuery(jpql, Produto.class);
+        if(nome != null && !nome.isEmpty()){
+            query.setParameter("nome", nome);
+        }
+        if(preco != null){
+            query.setParameter("preco", preco);
+        }
+        return query.getResultList();
     }
 }
